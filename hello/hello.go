3adea1a45a2a828c432a40659518eb1535b761e6
Alec Gillis <gillismail1213@gmail.com>
2022-02-07 18:31:39 -0500

    Go Tutorial: 2 - Call your code from module

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+
+    "example.com/greetings"
+) 
+
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println("Hello, World!")
+    fmt.Println(quote.Go())
+
+     // Get a greeting message and print it.
+     message := greetings.Hello("Gladys")
+     fmt.Println(message)
+}
